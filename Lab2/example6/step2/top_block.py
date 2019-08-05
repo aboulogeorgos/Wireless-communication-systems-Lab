b653dbbf4caf1d029af80189a67036a36106b5ca
aboulogeorgos <al.mpoulogeorgos@gmail.com>
2019-08-05 09:57:19 +0300

    [Lab2Example6] Step1-Updated, Step2-Added

--- Lab2/example6/step2/top_block.py
+++ Lab2/example6/step2/top_block.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: AM singal @RX
-# Author: Alexandros-Apostolos A. Boulogeorgos
-# Generated: Mon Aug  5 09:55:40 2019
+# Title: Top Block
+# Generated: Mon Aug  5 09:55:09 2019
 ##################################################
 
 if __name__ == '__main__':
@@ -19,6 +18,7 @@ if __name__ == '__main__':
 
 from gnuradio import blocks
 from gnuradio import eng_notation
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
@@ -30,15 +30,16 @@ from optparse import OptionParser
 import wx
 
 
-class am_rx_signal(grc_wxgui.top_block_gui):
+class top_block(grc_wxgui.top_block_gui):
 
     def __init__(self):
-        grc_wxgui.top_block_gui.__init__(self, title="AM singal @RX")
+        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
 
         ##################################################
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 256000
+        self.resamp_factor = resamp_factor = 4
 
         ##################################################
         # Blocks
@@ -50,7 +51,7 @@ class am_rx_signal(grc_wxgui.top_block_gui):
         	y_divs=10,
         	ref_level=0,
         	ref_scale=2.0,
-        	sample_rate=samp_rate,
+        	sample_rate=samp_rate/resamp_factor,
         	fft_size=1024,
         	fft_rate=15,
         	average=False,
@@ -59,25 +60,40 @@ class am_rx_signal(grc_wxgui.top_block_gui):
         	peak_hold=False,
         )
         self.Add(self.wxgui_fftsink2_0.win)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=1,
+                decimation=resamp_factor,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate/resamp_factor,True)
         self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '/Users/ampoulog/Documents/gnuradio/Wireless-communication-systems-Lab/Lab2/example6/am_usrp710.dat', True)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))    
+        self.connect((self.blocks_file_source_0, 0), (self.rational_resampler_xxx_0, 0))    
         self.connect((self.blocks_throttle_0, 0), (self.wxgui_fftsink2_0, 0))    
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_throttle_0, 0))    
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate/self.resamp_factor)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate/self.resamp_factor)
+
+    def get_resamp_factor(self):
+        return self.resamp_factor
+
+    def set_resamp_factor(self, resamp_factor):
+        self.resamp_factor = resamp_factor
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate/self.resamp_factor)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate/self.resamp_factor)
 
 
-def main(top_block_cls=am_rx_signal, options=None):
+def main(top_block_cls=top_block, options=None):
 
     tb = top_block_cls()
     tb.Start(True)
