5b2d68a09efe29974b0f82a109fad4ceff62000c
aboulogeorgos <al.mpoulogeorgos@gmail.com>
2019-08-05 17:43:09 +0300

    [Lab0Example5] Added

---
+++ Lab0/example5/delay_example.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Delay Example
+# Author: Alexandros-Apostolos A. Boulogeorgos
+# Generated: Mon Aug  5 17:42:09 2019
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from gnuradio.wxgui import forms
+from gnuradio.wxgui import scopesink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import wx
+
+
+class delay_example(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Delay Example")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 32000
+        self.f0 = f0 = 1000
+        self.delay = delay = 2
+        self.constant = constant = 1
+
+        ##################################################
+        # Blocks
+        ##################################################
+        _delay_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._delay_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_delay_sizer,
+        	value=self.delay,
+        	callback=self.set_delay,
+        	label='delay',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._delay_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_delay_sizer,
+        	value=self.delay,
+        	callback=self.set_delay,
+        	minimum=0,
+        	maximum=100,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_delay_sizer)
+        _constant_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._constant_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_constant_sizer,
+        	value=self.constant,
+        	callback=self.set_constant,
+        	label='Delayed multiply constant',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._constant_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_constant_sizer,
+        	value=self.constant,
+        	callback=self.set_constant,
+        	minimum=0,
+        	maximum=1,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_constant_sizer)
+        self.wxgui_scopesink2_0 = scopesink2.scope_sink_f(
+        	self.GetWin(),
+        	title='Scope Plot',
+        	sample_rate=samp_rate,
+        	v_scale=0,
+        	v_offset=0,
+        	t_scale=0,
+        	ac_couple=False,
+        	xy_mode=False,
+        	num_inputs=2,
+        	trig_mode=wxgui.TRIG_MODE_AUTO,
+        	y_axis_label='Counts',
+        )
+        self.Add(self.wxgui_scopesink2_0.win)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((constant, ))
+        self.blocks_delay_0 = blocks.delay(gr.sizeof_int*1, delay)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, f0, 1, 0)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle_0, 0))    
+        self.connect((self.blocks_delay_0, 0), (self.blocks_multiply_const_vxx_0, 0))    
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.wxgui_scopesink2_0, 1))    
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_delay_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.wxgui_scopesink2_0, 0))    
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.wxgui_scopesink2_0.set_sample_rate(self.samp_rate)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+
+    def get_f0(self):
+        return self.f0
+
+    def set_f0(self, f0):
+        self.f0 = f0
+        self.analog_sig_source_x_0.set_frequency(self.f0)
+
+    def get_delay(self):
+        return self.delay
+
+    def set_delay(self, delay):
+        self.delay = delay
+        self._delay_slider.set_value(self.delay)
+        self._delay_text_box.set_value(self.delay)
+        self.blocks_delay_0.set_dly(self.delay)
+
+    def get_constant(self):
+        return self.constant
+
+    def set_constant(self, constant):
+        self.constant = constant
+        self._constant_slider.set_value(self.constant)
+        self._constant_text_box.set_value(self.constant)
+        self.blocks_multiply_const_vxx_0.set_k((self.constant, ))
+
+
+def main(top_block_cls=delay_example, options=None):
+
+    tb = top_block_cls()
+    tb.Start(True)
+    tb.Wait()
+
+
+if __name__ == '__main__':
+    main()
