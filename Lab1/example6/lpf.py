83859e6011c46a2a135c3936d669fe8a1d49830b
aboulogeorgos <al.mpoulogeorgos@gmail.com>
2019-08-06 15:30:08 +0300

    [Lab1Example1] Added

---
+++ Lab1/example6/lpf.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: low_pass_filter
+# Author: Alexandros-Apostolos A. Boulogeorgos
+# Generated: Tue Aug  6 15:29:44 2019
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import fftsink2
+from gnuradio.wxgui import scopesink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import wx
+
+
+class lpf(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="low_pass_filter")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 10000
+        self.f1 = f1 = 800
+        self.f0 = f0 = 1000
+        self.amp1 = amp1 = 0.5
+        self.amp0 = amp0 = 0.5
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.nbook = self.nbook = wx.Notebook(self.GetWin(), style=wx.NB_TOP)
+        self.nbook.AddPage(grc_wxgui.Panel(self.nbook), "tab1")
+        self.nbook.AddPage(grc_wxgui.Panel(self.nbook), "tab2")
+        self.Add(self.nbook)
+        self.wxgui_scopesink2_0_0 = scopesink2.scope_sink_f(
+        	self.nbook.GetPage(0).GetWin(),
+        	title='Scope Plot',
+        	sample_rate=samp_rate,
+        	v_scale=0,
+        	v_offset=0,
+        	t_scale=0,
+        	ac_couple=False,
+        	xy_mode=False,
+        	num_inputs=1,
+        	trig_mode=wxgui.TRIG_MODE_AUTO,
+        	y_axis_label='Counts',
+        )
+        self.nbook.GetPage(0).Add(self.wxgui_scopesink2_0_0.win)
+        self.wxgui_scopesink2_0 = scopesink2.scope_sink_f(
+        	self.nbook.GetPage(1).GetWin(),
+        	title='Scope Plot',
+        	sample_rate=samp_rate,
+        	v_scale=0,
+        	v_offset=0,
+        	t_scale=0,
+        	ac_couple=False,
+        	xy_mode=False,
+        	num_inputs=1,
+        	trig_mode=wxgui.TRIG_MODE_AUTO,
+        	y_axis_label='Counts',
+        )
+        self.nbook.GetPage(1).Add(self.wxgui_scopesink2_0.win)
+        self.wxgui_fftsink2_0_0 = fftsink2.fft_sink_f(
+        	self.nbook.GetPage(0).GetWin(),
+        	baseband_freq=0,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title='FFT Plot',
+        	peak_hold=False,
+        )
+        self.nbook.GetPage(0).Add(self.wxgui_fftsink2_0_0.win)
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_f(
+        	self.nbook.GetPage(1).GetWin(),
+        	baseband_freq=0,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title='FFT Plot',
+        	peak_hold=False,
+        )
+        self.nbook.GetPage(1).Add(self.wxgui_fftsink2_0.win)
+        self.low_pass_filter_0 = filter.fir_filter_fff(1, firdes.low_pass(
+        	1, samp_rate, 1000, 200, firdes.WIN_HAMMING, 6.76))
+        self.blocks_throttle_0_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
+        self.analog_sig_source_x_0_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, f1, amp1, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, f0, amp0, 0)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 0))    
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0, 1))    
+        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_throttle_0_0, 0))    
+        self.connect((self.blocks_multiply_xx_0, 0), (self.low_pass_filter_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.wxgui_fftsink2_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.wxgui_scopesink2_0, 0))    
+        self.connect((self.blocks_throttle_0_0, 0), (self.wxgui_fftsink2_0_0, 0))    
+        self.connect((self.blocks_throttle_0_0, 0), (self.wxgui_scopesink2_0_0, 0))    
+        self.connect((self.low_pass_filter_0, 0), (self.blocks_throttle_0, 0))    
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.wxgui_scopesink2_0_0.set_sample_rate(self.samp_rate)
+        self.wxgui_scopesink2_0.set_sample_rate(self.samp_rate)
+        self.wxgui_fftsink2_0_0.set_sample_rate(self.samp_rate)
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 1000, 200, firdes.WIN_HAMMING, 6.76))
+        self.blocks_throttle_0_0.set_sample_rate(self.samp_rate)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+
+    def get_f1(self):
+        return self.f1
+
+    def set_f1(self, f1):
+        self.f1 = f1
+        self.analog_sig_source_x_0_0.set_frequency(self.f1)
+
+    def get_f0(self):
+        return self.f0
+
+    def set_f0(self, f0):
+        self.f0 = f0
+        self.analog_sig_source_x_0.set_frequency(self.f0)
+
+    def get_amp1(self):
+        return self.amp1
+
+    def set_amp1(self, amp1):
+        self.amp1 = amp1
+        self.analog_sig_source_x_0_0.set_amplitude(self.amp1)
+
+    def get_amp0(self):
+        return self.amp0
+
+    def set_amp0(self, amp0):
+        self.amp0 = amp0
+        self.analog_sig_source_x_0.set_amplitude(self.amp0)
+
+
+def main(top_block_cls=lpf, options=None):
+
+    tb = top_block_cls()
+    tb.Start(True)
+    tb.Wait()
+
+
+if __name__ == '__main__':
+    main()
