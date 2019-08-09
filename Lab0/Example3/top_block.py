75c5fc7519741f0cc435b2ba2ddcea4cf5236fbd
aboulogeorgos <al.mpoulogeorgos@gmail.com>
2019-08-09 07:26:55 +0300

    [Lab0Example3] Update & ScreenCapture Added

--- Lab0/Example3/top_block.py
+++ Lab0/Example3/top_block.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: Top Block
-# Generated: Wed Aug  7 14:38:29 2019
+# Title: Combination of two signal sources
+# Author: Alexandros-Apostolos A. Boulogeorgos
+# Generated: Fri Aug  9 07:26:16 2019
 ##################################################
 
 if __name__ == '__main__':
@@ -17,6 +18,7 @@ if __name__ == '__main__':
             print "Warning: failed to XInitThreads()"
 
 from PyQt4 import Qt
+from PyQt4.QtCore import QObject, pyqtSlot
 from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import eng_notation
@@ -32,9 +34,9 @@ import sys
 class top_block(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Top Block")
+        gr.top_block.__init__(self, "Combination of two signal sources")
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Top Block")
+        self.setWindowTitle("Combination of two signal sources")
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
         except:
@@ -57,11 +59,137 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
+        self.waveform2 = waveform2 = 102
+        self.waveform1 = waveform1 = 102
         self.samp_rate = samp_rate = 48000
 
         ##################################################
         # Blocks
         ##################################################
+        self._waveform2_options = (101, 102, 103, 104, 105, )
+        self._waveform2_labels = ('Sine', 'Cosine', 'Rectangular', 'Triangular', 'Saw tooth', )
+        self._waveform2_tool_bar = Qt.QToolBar(self)
+        self._waveform2_tool_bar.addWidget(Qt.QLabel('Waveform of signal source 2'+": "))
+        self._waveform2_combo_box = Qt.QComboBox()
+        self._waveform2_tool_bar.addWidget(self._waveform2_combo_box)
+        for label in self._waveform2_labels: self._waveform2_combo_box.addItem(label)
+        self._waveform2_callback = lambda i: Qt.QMetaObject.invokeMethod(self._waveform2_combo_box, "setCurrentIndex", Qt.Q_ARG("int", self._waveform2_options.index(i)))
+        self._waveform2_callback(self.waveform2)
+        self._waveform2_combo_box.currentIndexChanged.connect(
+        	lambda i: self.set_waveform2(self._waveform2_options[i]))
+        self.top_grid_layout.addWidget(self._waveform2_tool_bar, 0,1,1,1)
+        self._waveform1_options = (101, 102, 103, 104, 105, )
+        self._waveform1_labels = ('Sine', 'Cosine', 'Rectangular', 'Triangular', 'Saw tooth', )
+        self._waveform1_tool_bar = Qt.QToolBar(self)
+        self._waveform1_tool_bar.addWidget(Qt.QLabel('Waveform of signal source 1'+": "))
+        self._waveform1_combo_box = Qt.QComboBox()
+        self._waveform1_tool_bar.addWidget(self._waveform1_combo_box)
+        for label in self._waveform1_labels: self._waveform1_combo_box.addItem(label)
+        self._waveform1_callback = lambda i: Qt.QMetaObject.invokeMethod(self._waveform1_combo_box, "setCurrentIndex", Qt.Q_ARG("int", self._waveform1_options.index(i)))
+        self._waveform1_callback(self.waveform1)
+        self._waveform1_combo_box.currentIndexChanged.connect(
+        	lambda i: self.set_waveform1(self._waveform1_options[i]))
+        self.top_grid_layout.addWidget(self._waveform1_tool_bar, 0,0,1,1)
+        self.qtgui_time_sink_x_0_0 = qtgui.time_sink_c(
+        	1024, #size
+        	samp_rate, #samp_rate
+        	"", #name
+        	1 #number of inputs
+        )
+        self.qtgui_time_sink_x_0_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0_0.set_y_axis(-1, 1)
+        
+        self.qtgui_time_sink_x_0_0.set_y_label('Amplitude', "")
+        
+        self.qtgui_time_sink_x_0_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0_0.enable_grid(False)
+        self.qtgui_time_sink_x_0_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0_0.enable_control_panel(False)
+        
+        if not True:
+          self.qtgui_time_sink_x_0_0.disable_legend()
+        
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "blue"]
+        styles = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        
+        for i in xrange(2*1):
+            if len(labels[i]) == 0:
+                if(i % 2 == 0):
+                    self.qtgui_time_sink_x_0_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
+                else:
+                    self.qtgui_time_sink_x_0_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
+            else:
+                self.qtgui_time_sink_x_0_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0_0.set_line_alpha(i, alphas[i])
+        
+        self._qtgui_time_sink_x_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_0_win, 1,1,1,1)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
+        	1024, #size
+        	samp_rate, #samp_rate
+        	"", #name
+        	1 #number of inputs
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+        
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+        
+        self.qtgui_time_sink_x_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        
+        if not True:
+          self.qtgui_time_sink_x_0.disable_legend()
+        
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "blue"]
+        styles = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        
+        for i in xrange(2*1):
+            if len(labels[i]) == 0:
+                if(i % 2 == 0):
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
+                else:
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+        
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win, 1,0,1,1)
         self.qtgui_sink_x_0 = qtgui.sink_c(
         	1024, #fftsize
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
@@ -83,14 +211,16 @@ class top_block(gr.top_block, Qt.QWidget):
           
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
-        self.analog_sig_source_x_0_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 800, 0.500, 0)
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 1000, 0.500, 0)
+        self.analog_sig_source_x_0_0 = analog.sig_source_c(samp_rate, waveform2, 800, 0.500, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, waveform1, 1000, 0.500, 0)
 
         ##################################################
         # Connections
         ##################################################
         self.connect((self.analog_sig_source_x_0, 0), (self.blocks_add_xx_0, 0))    
+        self.connect((self.analog_sig_source_x_0, 0), (self.qtgui_time_sink_x_0, 0))    
         self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_add_xx_0, 1))    
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.qtgui_time_sink_x_0_0, 0))    
         self.connect((self.blocks_add_xx_0, 0), (self.blocks_throttle_0, 0))    
         self.connect((self.blocks_throttle_0, 0), (self.qtgui_sink_x_0, 0))    
 
@@ -99,11 +229,29 @@ class top_block(gr.top_block, Qt.QWidget):
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
+    def get_waveform2(self):
+        return self.waveform2
+
+    def set_waveform2(self, waveform2):
+        self.waveform2 = waveform2
+        self._waveform2_callback(self.waveform2)
+        self.analog_sig_source_x_0_0.set_waveform(self.waveform2)
+
+    def get_waveform1(self):
+        return self.waveform1
+
+    def set_waveform1(self, waveform1):
+        self.waveform1 = waveform1
+        self._waveform1_callback(self.waveform1)
+        self.analog_sig_source_x_0.set_waveform(self.waveform1)
+
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.qtgui_time_sink_x_0_0.set_samp_rate(self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
         self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
         self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
