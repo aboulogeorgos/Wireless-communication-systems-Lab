ddfee842b65c5cc22b81bd5fd44a6ac3a1a1d856
aboulogeorgos <al.mpoulogeorgos@gmail.com>
2019-08-10 08:51:26 +0300

    [Lab2Example7Step1] Update & ScreenCapture Added

--- Lab2/example7/step1/ssb_step1.py
+++ Lab2/example7/step1/ssb_step1.py
@@ -5,7 +5,7 @@
 # Title: SSB step1
 # Author: Alexandros-Apostolos A. Boulogeorgos
 # Description: Scope plot of a cosine source
-# Generated: Mon Aug  5 10:58:25 2019
+# Generated: Sat Aug 10 08:50:06 2019
 ##################################################
 
 if __name__ == '__main__':
@@ -24,8 +24,11 @@ from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
 from gnuradio.filter import firdes
+from gnuradio.wxgui import fftsink2
 from gnuradio.wxgui import scopesink2
+from gnuradio.wxgui import waterfallsink2
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
 import wx
@@ -44,6 +47,20 @@ class ssb_step1(grc_wxgui.top_block_gui):
         ##################################################
         # Blocks
         ##################################################
+        self.wxgui_waterfallsink2_0 = waterfallsink2.waterfall_sink_f(
+        	self.GetWin(),
+        	baseband_freq=0,
+        	dynamic_range=100,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=512,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title='Waterfall Plot',
+        )
+        self.Add(self.wxgui_waterfallsink2_0.win)
         self.wxgui_scopesink2_0 = scopesink2.scope_sink_f(
         	self.GetWin(),
         	title='Scope Plot',
@@ -58,6 +75,22 @@ class ssb_step1(grc_wxgui.top_block_gui):
         	y_axis_label='Counts',
         )
         self.Add(self.wxgui_scopesink2_0.win)
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_f(
+        	self.GetWin(),
+        	baseband_freq=0,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title='FFT Plot',
+        	peak_hold=False,
+        )
+        self.Add(self.wxgui_fftsink2_0.win)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
         self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 1000, 1, 0)
 
@@ -65,14 +98,18 @@ class ssb_step1(grc_wxgui.top_block_gui):
         # Connections
         ##################################################
         self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.wxgui_fftsink2_0, 0))    
         self.connect((self.blocks_throttle_0, 0), (self.wxgui_scopesink2_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.wxgui_waterfallsink2_0, 0))    
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.wxgui_waterfallsink2_0.set_sample_rate(self.samp_rate)
         self.wxgui_scopesink2_0.set_sample_rate(self.samp_rate)
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
 
